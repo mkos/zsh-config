3f10007e72079bba9c9a94221ef51ce9dd814d79
Michal Kosinski <kosinski.michal@gmail.com>
2015-04-19 19:46:12 +0200

    fixed tmux_autostart() for non-login shells

--- util.zsh
+++ util.zsh
@@ -16,19 +16,25 @@ function gitall {
   done
 }
 
-# shamelessly copied from http://blog.thelinuxkid.com/2013/06/automatically-start-tmux-on-ssh.html
+# original version from http://blog.thelinuxkid.com/2013/06/automatically-start-tmux-on-ssh.html
+# enhanced with checking if shell is login shell (so it won't start tmux
+# when working in X terminal)
+#
 # starts tmux automatically on SSH login and logouts after detach.
 # If tmux session exists, attaches to it, creating new one otherwise.
 # this should be called from zshrc as a very last command
+
 function tmux_autostart() {
-    if [[ -z "$TMUX" ]]; then
-        tmux has-session &> /dev/null
-        if [ $? -eq 1 ]; then
-          exec tmux new
-          exit
-        else
-          exec tmux attach
-          exit
+    if [[ -o login ]]; then
+        if [[ -z "$TMUX" ]]; then
+            tmux has-session &> /dev/null
+            if [ $? -eq 1 ]; then
+              exec tmux new
+              exit
+            else
+              exec tmux attach
+              exit
+            fi
         fi
     fi
 }
