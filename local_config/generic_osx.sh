edcf76cc5a1d310735bfe65dcb6c6d2866ad917b
Michal Kosinski <kosinski.michal@gmail.com>
2016-04-23 02:33:13 +0200

    added config for gnu utils

--- local_config/generic_osx.sh
+++ local_config/generic_osx.sh
@@ -1,2 +1,16 @@
 export CLICOLOR=1
 
+# INSTALL GNU UTILS
+# brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt
+# coreutils
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+# findutils
+export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/findutils/libexec/gnuman:$MANPATH"
+# gnu-tar
+export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/gnu-tar/libexec/gnuman:$MANPATH"
+# gnu-sed
+export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
