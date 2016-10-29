0e82c775d2a6f7043ad114fad8c4617cef660976
Michal Kosinski <kosinski.michal@gmail.com>
2016-10-29 19:51:04 +0200

    update spark to 1.6

--- local_config/spark_osx.sh
+++ local_config/spark_osx.sh
@@ -1,5 +1,8 @@
 export SPARK_HOME="$(brew --prefix apache-spark)/libexec"
 export PYSPARKPATH="$SPARK_HOME/python"
-export PY4JPATH="$PYSPARKPATH/lib/py4j-0.8.2.1-src.zip"
+# for Spark 1.4
+#export PY4JPATH="$PYSPARKPATH/lib/py4j-0.8.2.1-src.zip"
+# for Spark 1.6
+export PY4JPATH="$PYSPARKPATH/lib/py4j-0.9-src.zip"
 export PYTHONPATH="$PY4JPATH:$PYSPARKPATH:$PYTHONPATH"
 export MASTER="local"
