3c149755bcc57fdc3b90156bfe16a5c0d078da02
Michal Kosinski <kosinski.michal@gmail.com>
2017-02-21 18:13:58 +0100

    static spark path for OSX

--- local_config/spark_osx.sh
+++ local_config/spark_osx.sh
@@ -1,4 +1,4 @@
-export SPARK_HOME="$(brew --prefix apache-spark)/libexec"
+export SPARK_HOME="/usr/local/Cellar/apache-spark/1.6.1/libexec"
 export PYSPARKPATH="$SPARK_HOME/python"
 # for Spark 1.4
 #export PY4JPATH="$PYSPARKPATH/lib/py4j-0.8.2.1-src.zip"
